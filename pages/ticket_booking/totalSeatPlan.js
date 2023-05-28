6bd2f78a581179eb4147222ca69f046b5231b2a5
rakibul667s <rakibulislam667s@gmail.com>
2023-05-28 17:29:11 +0600

    type change

--- pages/ticket_booking/totalSeatPlan.js
+++ pages/ticket_booking/totalSeatPlan.js
@@ -23,13 +23,13 @@ const TotalSeatPlan = () => {
           </div>
           <div className="grid grid-cols-3 md:gap-10 gap-4 md:mt-8 md:mb-0 mb-4 text-white justify-center">
             <div className="bg-gray-600 h-10 md:w-40 w-24 flex items-center justify-center rounded-sm">
-              C: 100
+              C: 100tk
             </div>
             <div className="bg-red-600 h-10 md:w-40  w-24 flex items-center justify-center rounded-sm">
-              S: 200
+              S: 200tk
             </div>
             <div className="bg-teal-600 h-10 md:w-40 w-24 flex items-center justify-center rounded-sm">
-              P: 300
+              P: 300tk
             </div>
           </div>
         </div>
